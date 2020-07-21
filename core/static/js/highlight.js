cb118ba2784e086620e5f70c4b4bba0d70ad94af
victor-s-santos <victor.s.silva@unesp.br>
2020-07-21 17:27:26 -0300

    melhor estilização aplicada

--- core/static/js/highlight.js
+++ core/static/js/highlight.js
@@ -16,17 +16,8 @@ function highlight(newElem, oldElem){
   });
   newElem.html(text); 
 }
-* css
+
 .highlight {background-color: #B4D5FF}
-* html
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <p id="old">this is number 123</p>
 <p id="new">that is number 124</p>
-
-e aplicar a função highlight sobre todas as linhas:
-$("table tr").each(function(index){
-    a = $(this).find("td");
-    first_td = a.eq(0);
-    second_td = a.eq(1);    
-    highlight(second_td, first_td);
-  });
