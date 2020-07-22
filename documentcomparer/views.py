80ce57b0410e89caff010864dccdbaa3136b1a97
victor-s-santos <victor.s.silva@unesp.br>
2020-07-22 12:01:45 -0300

    aplicando a gambiarra das gambiarras

--- documentcomparer/views.py
+++ documentcomparer/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 import pandas as pd
+import os
+import sys
 from django.shortcuts import render
 from .forms import UploadFileForm
 from .CheckFiles import CheckFiles
@@ -8,7 +10,7 @@ def comparador(request):
     form = UploadFileForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
-            check = CheckFiles(form.cleaned_data["arquivo1"], form.cleaned_data["arquivo2"]).verifica_tamanho()
+            check = CheckFiles(os.path.join(sys.path[0], "PrestacaoServico_Canon.docx"), form.cleaned_data["arquivo2"]).verifica_tamanho()
             check.columns = ['arquivo1', 'arquivo2']
             #check.sort_values("Arquivo1", inplace=True)
             if type(check) == str:
