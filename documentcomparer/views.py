69b3e89af890932ba29e6584c1af1bcee4bcbbf0
victor-s-santos <victor.s.silva@unesp.br>
2020-07-22 12:21:02 -0300

    corrigindo a gambiarra para o heroku

--- documentcomparer/views.py
+++ documentcomparer/views.py
@@ -5,13 +5,14 @@ import sys
 from django.shortcuts import render
 from .forms import UploadFileForm
 from .CheckFiles import CheckFiles
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+arquivo_principal = os.path.join(BASE_DIR, "PrestacaoServico_Canon.docx")
 def comparador(request):
     form = UploadFileForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
-            check = CheckFiles(os.path.join(sys.path[0], "PrestacaoServico_Canon.docx"), form.cleaned_data["arquivo2"]).verifica_tamanho()
-            check.columns = ['arquivo1', 'arquivo2']
+            check = CheckFiles(arquivo_principal, form.cleaned_data["arquivo2"]).verifica_tamanho()
+            #check.columns = ['arquivo1', 'arquivo2']
             #check.sort_values("Arquivo1", inplace=True)
             if type(check) == str:
                 return render(request, 'arquivos_iguais.html')
