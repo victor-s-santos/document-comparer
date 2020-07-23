2bb9a2fa339a45fdf2ae385be8e7809f7c3619dd
victor-s-santos <victor.s.silva@unesp.br>
2020-07-23 15:30:52 -0300

    aplicando testes à página home

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+class InitTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must returns status_code 200"""
+        response = self.client.get('/')
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template(self):
+        """must use index.html"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'index.html')
