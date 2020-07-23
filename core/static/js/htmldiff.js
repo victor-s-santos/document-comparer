1b40e8171ed0958ba2888e401b731bd9d80f4be7
victor-s-santos <victor.s.silva@unesp.br>
2020-07-23 09:28:20 -0300

    configura o javascript como arquivo estático

--- core/static/js/htmldiff.js
+++ core/static/js/htmldiff.js
@@ -1,1 +1,348 @@
-!function(){var e,n,t,r,i,f,_,a,o,s,u,h,l,c,d,b,p;o=function(e){return">"===e},s=function(e){return"<"===e},h=function(e){return/^\s+$/.test(e)},u=function(e){return/^\s*<[^>]+>\s*$/.test(e)},l=function(e){return!u(e)},e=function(){return function(e,n,t){this.start_in_before=e,this.start_in_after=n,this.length=t,this.end_in_before=this.start_in_before+this.length-1,this.end_in_after=this.start_in_after+this.length-1}}(),a=function(e){var n,t,r,i,f,_;for(f="char",t="",_=[],r=0,i=e.length;r<i;r++)switch(n=e[r],f){case"tag":o(n)?(t+=">",_.push(t),t="",f=h(n)?"whitespace":"char"):t+=n;break;case"char":s(n)?(t&&_.push(t),t="<",f="tag"):/\s/.test(n)?(t&&_.push(t),t=n,f="whitespace"):/[\w\#@]+/i.test(n)?t+=n:(t&&_.push(t),t=n);break;case"whitespace":s(n)?(t&&_.push(t),t="<",f="tag"):h(n)?t+=n:(t&&_.push(t),t=n,f="char");break;default:throw new Error("Unknown mode "+f)}return t&&_.push(t),_},f=function(n,t,r,i,f,_,a){var o,s,u,h,l,c,d,b,p,g,w,v,k,m,y;for(s=i,o=_,u=0,w={},c=h=m=i,y=f;m<=y?h<y:h>y;c=m<=y?++h:--h){for(k={},d=0,b=(p=r[n[c]]).length;d<b;d++)if(!((l=p[d])<_)){if(l>=a)break;null==w[l-1]&&(w[l-1]=0),v=w[l-1]+1,k[l]=v,v>u&&(s=c-v+1,o=l-v+1,u=v)}w=k}return 0!==u&&(g=new e(s,o,u)),g},d=function(e,n,t,r,i,_,a,o){var s;return null!=(s=f(e,0,t,r,i,_,a))&&(r<s.start_in_before&&_<s.start_in_after&&d(e,n,t,r,s.start_in_before,_,s.start_in_after,o),o.push(s),s.end_in_before<=i&&s.end_in_after<=a&&d(e,n,t,s.end_in_before+1,i,s.end_in_after+1,a,o)),o},r=function(e){var n,t,r,i,f,_;if(null==e.find_these)throw new Error("params must have find_these key");if(null==e.in_these)throw new Error("params must have in_these key");for(r={},n=0,i=(f=e.find_these).length;n<i;n++)for(r[_=f[n]]=[],t=e.in_these.indexOf(_);-1!==t;)r[_].push(t),t=e.in_these.indexOf(_,t+1);return r},_=function(e,n){var t,i;return i=[],t=r({find_these:e,in_these:n}),d(e,n,t,0,e.length,0,n.length,i)},n=function(n,t){var r,i,f,a,o,s,u,h,l,c,d,b,p,g,w,v;if(null==n)throw new Error("before_tokens?");if(null==t)throw new Error("after_tokens?");for(w=g=0,p=[],r={"false,false":"replace","true,false":"insert","false,true":"delete","true,true":"none"},(d=_(n,t)).push(new e(n.length,t.length,0)),a=f=0,h=d.length;f<h;a=++f)"none"!==(i=r[[w===(c=d[a]).start_in_before,g===c.start_in_after].toString()])&&p.push({action:i,start_in_before:w,end_in_before:"insert"!==i?c.start_in_before-1:void 0,start_in_after:g,end_in_after:"delete"!==i?c.start_in_after-1:void 0}),0!==c.length&&p.push({action:"equal",start_in_before:c.start_in_before,end_in_before:c.end_in_before,start_in_after:c.start_in_after,end_in_after:c.end_in_after}),w=c.end_in_before+1,g=c.end_in_after+1;for(v=[],u={action:"none"},o=function(e){return"equal"===e.action&&(e.end_in_before-e.start_in_before==0&&/^\s$/.test(n.slice(e.start_in_before,+e.end_in_before+1||9e9)))},s=0,l=p.length;s<l;s++)o(b=p[s])&&"replace"===u.action||"replace"===b.action&&"replace"===u.action?(u.end_in_before=b.end_in_before,u.end_in_after=b.end_in_after):(v.push(b),u=b);return v},t=function(e,n,t){var r,i,f,_,a,o;for(_=void 0,f=i=0,a=(n=n.slice(e,+n.length+1||9e9)).length;i<a&&(o=n[f],!0===(r=t(o))&&(_=f),!1!==r);f=++i);return null!=_?n.slice(0,+_+1||9e9):[]},p=function(e,n){var r,i,f,_,a;for(_="",f=0,r=n.length;;){if(f>=r)break;if(i=t(f,n,l),f+=i.length,0!==i.length&&(_+="<"+e+">"+i.join("")+"</"+e+">"),f>=r)break;f+=(a=t(f,n,u)).length,_+=a.join("")}return _},(c={equal:function(e,n,t){return n.slice(e.start_in_before,+e.end_in_before+1||9e9).join("")},insert:function(e,n,t){var r;return r=t.slice(e.start_in_after,+e.end_in_after+1||9e9),p("ins",r)},delete:function(e,n,t){var r;return r=n.slice(e.start_in_before,+e.end_in_before+1||9e9),p("del",r)}}).replace=function(e,n,t){return c.delete(e,n,t)+c.insert(e,n,t)},b=function(e,n,t){var r,i,f,_;for(_="",r=0,i=t.length;r<i;r++)f=t[r],_+=c[f.action](f,e,n);return _},(i=function(e,t){var r;return e===t?e:(e=a(e),t=a(t),r=n(e,t),b(e,t,r))}).html_to_tokens=a,i.find_matching_blocks=_,_.find_match=f,_.create_index=r,i.calculate_operations=n,i.render_operations=b,"function"==typeof define?define([],function(){return i}):"undefined"!=typeof module&&null!==module?module.exports=i:"undefined"!=typeof window&&(window.htmldiff=i)}();
+var Match, calculate_operations, consecutive_where, create_index, diff, find_match, find_matching_blocks, html_to_tokens, is_end_of_tag, is_start_of_tag, is_tag, is_whitespace, isnt_tag, op_map, recursively_find_matching_blocks, render_operations, wrap;
+is_end_of_tag = function(char) {
+  return char === '>';
+};
+is_start_of_tag = function(char) {
+  return char === '<';
+};
+is_whitespace = function(char) {
+  return /^\s+$/.test(char);
+};
+is_tag = function(token) {
+  return /^\s*<[^>]+>\s*$/.test(token);
+};
+isnt_tag = function(token) {
+  return !is_tag(token);
+};
+Match = class Match {
+  constructor(start_in_before1, start_in_after1, length1) {
+    this.start_in_before = start_in_before1;
+    this.start_in_after = start_in_after1;
+    this.length = length1;
+    this.end_in_before = (this.start_in_before + this.length) - 1;
+    this.end_in_after = (this.start_in_after + this.length) - 1;
+  }
+};
+html_to_tokens = function(html) {
+  var char, current_word, i, len, mode, words;
+  mode = 'char';
+  current_word = '';
+  words = [];
+  for (i = 0, len = html.length; i < len; i++) {
+    char = html[i];
+    switch (mode) {
+      case 'tag':
+        if (is_end_of_tag(char)) {
+          current_word += '>';
+          words.push(current_word);
+          current_word = '';
+          if (is_whitespace(char)) {
+            mode = 'whitespace';
+          } else {
+            mode = 'char';
+          }
+        } else {
+          current_word += char;
+        }
+        break;
+      case 'char':
+        if (is_start_of_tag(char)) {
+          if (current_word) {
+            words.push(current_word);
+          }
+          current_word = '<';
+          mode = 'tag';
+        } else if (/\s/.test(char)) {
+          if (current_word) {
+            words.push(current_word);
+          }
+          current_word = char;
+          mode = 'whitespace';
+        } else if (/[\w\#@]+/i.test(char)) {
+          current_word += char;
+        } else {
+          if (current_word) {
+            words.push(current_word);
+          }
+          current_word = char;
+        }
+        break;
+      case 'whitespace':
+        if (is_start_of_tag(char)) {
+          if (current_word) {
+            words.push(current_word);
+          }
+          current_word = '<';
+          mode = 'tag';
+        } else if (is_whitespace(char)) {
+          current_word += char;
+        } else {
+          if (current_word) {
+            words.push(current_word);
+          }
+          current_word = char;
+          mode = 'char';
+        }
+        break;
+      default:
+        throw new Error(`Unknown mode ${mode}`);
+    }
+  }
+  if (current_word) {
+    words.push(current_word);
+  }
+  return words;
+};
+find_match = function(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, start_in_before, end_in_before, start_in_after, end_in_after) {
+  var best_match_in_after, best_match_in_before, best_match_length, i, index_in_after, index_in_before, j, len, locations_in_after, looking_for, match, match_length_at, new_match_length, new_match_length_at, ref, ref1;
+  best_match_in_before = start_in_before;
+  best_match_in_after = start_in_after;
+  best_match_length = 0;
+  match_length_at = {};
+  for (index_in_before = i = ref = start_in_before, ref1 = end_in_before;
+    (ref <= ref1 ? i < ref1 : i > ref1); index_in_before = ref <= ref1 ? ++i : --i) {
+    new_match_length_at = {};
+    looking_for = before_tokens[index_in_before];
+    locations_in_after = index_of_before_locations_in_after_tokens[looking_for];
+    for (j = 0, len = locations_in_after.length; j < len; j++) {
+      index_in_after = locations_in_after[j];
+      if (index_in_after < start_in_after) {
+        continue;
+      }
+      if (index_in_after >= end_in_after) {
+        break;
+      }
+      if (match_length_at[index_in_after - 1] == null) {
+        match_length_at[index_in_after - 1] = 0;
+      }
+      new_match_length = match_length_at[index_in_after - 1] + 1;
+      new_match_length_at[index_in_after] = new_match_length;
+      if (new_match_length > best_match_length) {
+        best_match_in_before = index_in_before - new_match_length + 1;
+        best_match_in_after = index_in_after - new_match_length + 1;
+        best_match_length = new_match_length;
+      }
+    }
+    match_length_at = new_match_length_at;
+  }
+  if (best_match_length !== 0) {
+    match = new Match(best_match_in_before, best_match_in_after, best_match_length);
+  }
+  return match;
+};
+recursively_find_matching_blocks = function(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, start_in_before, end_in_before, start_in_after, end_in_after, matching_blocks) {
+  var match;
+  match = find_match(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, start_in_before, end_in_before, start_in_after, end_in_after);
+  if (match != null) {
+    if (start_in_before < match.start_in_before && start_in_after < match.start_in_after) {
+      recursively_find_matching_blocks(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, start_in_before, match.start_in_before, start_in_after, match.start_in_after, matching_blocks);
+    }
+    matching_blocks.push(match);
+    if (match.end_in_before <= end_in_before && match.end_in_after <= end_in_after) {
+      recursively_find_matching_blocks(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, match.end_in_before + 1, end_in_before, match.end_in_after + 1, end_in_after, matching_blocks);
+    }
+  }
+  return matching_blocks;
+};
+create_index = function(p) {
+  var i, idx, index, len, ref, token;
+  if (p.find_these == null) {
+    throw new Error('params must have find_these key');
+  }
+  if (p.in_these == null) {
+    throw new Error('params must have in_these key');
+  }
+  index = {};
+  ref = p.find_these;
+  for (i = 0, len = ref.length; i < len; i++) {
+    token = ref[i];
+    index[token] = [];
+    idx = p.in_these.indexOf(token);
+    while (idx !== -1) {
+      index[token].push(idx);
+      idx = p.in_these.indexOf(token, idx + 1);
+    }
+  }
+  return index;
+};
+find_matching_blocks = function(before_tokens, after_tokens) {
+  var index_of_before_locations_in_after_tokens, matching_blocks;
+  matching_blocks = [];
+  index_of_before_locations_in_after_tokens = create_index({
+    find_these: before_tokens,
+    in_these: after_tokens
+  });
+  return recursively_find_matching_blocks(before_tokens, after_tokens, index_of_before_locations_in_after_tokens, 0, before_tokens.length, 0, after_tokens.length, matching_blocks);
+};
+calculate_operations = function(before_tokens, after_tokens) {
+  var action_map, action_up_to_match_positions, i, index, is_single_whitespace, j, last_op, len, len1, match, match_starts_at_current_position_in_after, match_starts_at_current_position_in_before, matches, op, operations, position_in_after, position_in_before, post_processed;
+  if (before_tokens == null) {
+    throw new Error('before_tokens?');
+  }
+  if (after_tokens == null) {
+    throw new Error('after_tokens?');
+  }
+  position_in_before = position_in_after = 0;
+  operations = [];
+  action_map = {
+    'false,false': 'replace',
+    'true,false': 'insert',
+    'false,true': 'delete',
+    'true,true': 'none'
+  };
+  matches = find_matching_blocks(before_tokens, after_tokens);
+  matches.push(new Match(before_tokens.length, after_tokens.length, 0));
+  for (index = i = 0, len = matches.length; i < len; index = ++i) {
+    match = matches[index];
+    match_starts_at_current_position_in_before = position_in_before === match.start_in_before;
+    match_starts_at_current_position_in_after = position_in_after === match.start_in_after;
+    action_up_to_match_positions = action_map[[match_starts_at_current_position_in_before, match_starts_at_current_position_in_after].toString()];
+    if (action_up_to_match_positions !== 'none') {
+      operations.push({
+        action: action_up_to_match_positions,
+        start_in_before: position_in_before,
+        end_in_before: (action_up_to_match_positions !== 'insert' ? match.start_in_before - 1 : void 0),
+        start_in_after: position_in_after,
+        end_in_after: (action_up_to_match_positions !== 'delete' ? match.start_in_after - 1 : void 0)
+      });
+    }
+    if (match.length !== 0) {
+      operations.push({
+        action: 'equal',
+        start_in_before: match.start_in_before,
+        end_in_before: match.end_in_before,
+        start_in_after: match.start_in_after,
+        end_in_after: match.end_in_after
+      });
+    }
+    position_in_before = match.end_in_before + 1;
+    position_in_after = match.end_in_after + 1;
+  }
+  post_processed = [];
+  last_op = {
+    action: 'none'
+  };
+  is_single_whitespace = function(op) {
+    if (op.action !== 'equal') {
+      return false;
+    }
+    if (op.end_in_before - op.start_in_before !== 0) {
+      return false;
+    }
+    return /^\s$/.test(before_tokens.slice(op.start_in_before, +op.end_in_before + 1 || 9e9));
+  };
+  for (j = 0, len1 = operations.length; j < len1; j++) {
+    op = operations[j];
+    if (((is_single_whitespace(op)) && last_op.action === 'replace') || (op.action === 'replace' && last_op.action === 'replace')) {
+      last_op.end_in_before = op.end_in_before;
+      last_op.end_in_after = op.end_in_after;
+    } else {
+      post_processed.push(op);
+      last_op = op;
+    }
+  }
+  return post_processed;
+};
+consecutive_where = function(start, content, predicate) {
+  var answer, i, index, last_matching_index, len, token;
+  content = content.slice(start, +content.length + 1 || 9e9);
+  last_matching_index = void 0;
+  for (index = i = 0, len = content.length; i < len; index = ++i) {
+    token = content[index];
+    answer = predicate(token);
+    if (answer === true) {
+      last_matching_index = index;
+    }
+    if (answer === false) {
+      break;
+    }
+  }
+  if (last_matching_index != null) {
+    return content.slice(0, +last_matching_index + 1 || 9e9);
+  }
+  return [];
+};
+wrap = function(tag, content) {
+  var length, non_tags, position, rendering, tags;
+  rendering = '';
+  position = 0;
+  length = content.length;
+  while (true) {
+    if (position >= length) {
+      break;
+    }
+    non_tags = consecutive_where(position, content, isnt_tag);
+    position += non_tags.length;
+    if (non_tags.length !== 0) {
+      rendering += `<${tag}>${non_tags.join('')}</${tag}>`;
+    }
+    if (position >= length) {
+      break;
+    }
+    tags = consecutive_where(position, content, is_tag);
+    position += tags.length;
+    rendering += tags.join('');
+  }
+  return rendering;
+};
+op_map = {
+  equal: function(op, before_tokens, after_tokens) {
+    return before_tokens.slice(op.start_in_before, +op.end_in_before + 1 || 9e9).join('');
+  },
+  insert: function(op, before_tokens, after_tokens) {
+    var val;
+    val = after_tokens.slice(op.start_in_after, +op.end_in_after + 1 || 9e9);
+    return wrap('ins', val);
+  },
+  delete: function(op, before_tokens, after_tokens) {
+    var val;
+    val = before_tokens.slice(op.start_in_before, +op.end_in_before + 1 || 9e9);
+    return wrap('del', val);
+  }
+};
+op_map.replace = function(op, before_tokens, after_tokens) {
+  return (op_map.delete(op, before_tokens, after_tokens)) + (op_map.insert(op, before_tokens, after_tokens));
+};
+render_operations = function(before_tokens, after_tokens, operations) {
+  var i, len, op, rendering;
+  rendering = '';
+  for (i = 0, len = operations.length; i < len; i++) {
+    op = operations[i];
+    rendering += op_map[op.action](op, before_tokens, after_tokens);
+  }
+  return rendering;
+};
+diff = function(before, after) {
+  var ops;
+  if (before === after) {
+    return before;
+  }
+  before = html_to_tokens(before);
+  after = html_to_tokens(after);
+  ops = calculate_operations(before, after);
+  return render_operations(before, after, ops);
+};
+diff.html_to_tokens = html_to_tokens;
+diff.find_matching_blocks = find_matching_blocks;
+find_matching_blocks.find_match = find_match;
+find_matching_blocks.create_index = create_index;
+diff.calculate_operations = calculate_operations;
+diff.render_operations = render_operations;
+if (typeof define === 'function') {
+  define([], function() {
+    return diff;
+  });
+} else if (typeof module !== "undefined" && module !== null) {
+  module.exports = diff;
+} else {
+  this.htmldiff = diff;
+}
+
+function myfunc(){
+  document.getElementById('btn').style.visibility='hidden';
+  var table = document.getElementsByTagName("tbody")[0];
+for(var i = 0; i < table.rows.length; i++) {
+    var cells = table.rows[i].cells;
+    cells[1].innerHTML = htmldiff(cells[0].innerHTML, cells[1].innerHTML);
+}
+}
