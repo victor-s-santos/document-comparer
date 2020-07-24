9796b37150492af3bfd033e44a7f4cfe8c04c083
victor-s-santos <victor.s.silva@unesp.br>
2020-07-24 17:11:32 -0300

    teste de template e de status code finalizado para a app core

--- core/tests.py
+++ core/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
 class InitTest(TestCase):
-    """GET / Must returns status code 200"""
+    def setUp(self):
+        self.response = self.client.get('/')
+
     def test_get(self):
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
+        """GET / Must returns status code 200"""
+        self.assertEqual(200, self.response.status_code)
     
     def test_template(self):
         """Must uses index.html"""
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(self.response, 'index.html')
