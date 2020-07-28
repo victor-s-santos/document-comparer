8403041ceb055b2e6d7a487176689c0c5601ea8b
victor-s-santos <victor.s.silva@unesp.br>
2020-07-28 10:13:02 -0300

    voltando a comparação de dois arquivos, nenhum fixo

--- documentcomparer/views.py
+++ documentcomparer/views.py
@@ -16,7 +16,8 @@ def comparador(request):
     form = UploadFileForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
-            check = CheckFiles(arquivo_principal, form.cleaned_data["arquivo2"]).verifica_tamanho()
+            check = CheckFiles(form.cleaned_data["arquivo1"], form.cleaned_data["arquivo2"]).verifica_tamanho()
+            #check = CheckFiles(arquivo_principal, form.cleaned_data["arquivo2"]).verifica_tamanho()
             #check.columns = ['arquivo1', 'arquivo2']
             #check.sort_values("Arquivo1", inplace=True)
             if type(check) == str:
