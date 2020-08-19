ac62bf13e4dc958f86b4d00e7f16928885c082af
victor-s-santos <victor.s.silva@unesp.br>
2020-08-19 10:43:44 -0300

    preparando para envio no github

--- documentcomparer/forms.py
+++ documentcomparer/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    arquivo1 = forms.FileField(required=False)
+    arquivo1 = forms.FileField()
     arquivo2 = forms.FileField()
--- documentcomparer/views.py
+++ documentcomparer/views.py
@@ -4,20 +4,16 @@ import sys
 from django.shortcuts import render
 from .forms import UploadFileForm
 from .utils.CheckFiles import CheckFiles
-#from .validador import FileExtensionValidator
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-"""BASE_DIR é necessário para criar o caminho dinâmico até o arquivo fixo de comparação"""
-arquivo_principal = os.path.join(BASE_DIR, "PrestacaoServico_JurisfAI BASEE.docx")
-"""Utilizando a lib os, gerei uma variável que é a string referente ao caminho do arquivo"""
+
+
 
 def comparador(request):
     form = UploadFileForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
-            #check = CheckFiles(form.cleaned_data["arquivo1"], form.cleaned_data["arquivo2"]).verifica_tamanho()
-            check = CheckFiles(arquivo_principal, form.cleaned_data["arquivo2"]).verifica_tamanho()
+            check = CheckFiles(form.cleaned_data["arquivo1"], form.cleaned_data["arquivo2"]).verifica_tamanho()
             if type(check) == str:
                 return render(request, 'arquivos_iguais.html')
             return render(request, 'tabela.html', {'check': check})
